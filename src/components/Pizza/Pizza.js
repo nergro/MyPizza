2be20edbc0836ad312d04428f55f4290fd37c90f
Nerijus <nerius10@gmail.com>
2019-05-18 00:57:27 +0300

    Adding responsive design

--- src/components/Pizza/Pizza.js
+++ src/components/Pizza/Pizza.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import './Pizza.scss';
+import TableImage from '../../assets/images/table.jpg';
 import Ingredients from './Ingredients/Ingredients';
 
 const pizza = props => {
+  const style = {
+    backgroundImage: `url(${TableImage})`,
+    backgroundSize: 'cover'
+  };
   return (
-    <div className='Pizza'>
+    <div className='Pizza' style={style}>
       <Ingredients toppings={props.toppings} />
     </div>
   );
