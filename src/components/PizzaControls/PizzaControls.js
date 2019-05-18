acc0755d50af81c3c6da84beecbdeedca12ddba1
Nerijus <nerius10@gmail.com>
2019-05-18 11:59:54 +0300

    Pizza size modifications

--- src/components/PizzaControls/PizzaControls.js
+++ src/components/PizzaControls/PizzaControls.js
@@ -13,22 +13,41 @@ const pizzaControls = props => {
           let classes = size;
           let style = {
             cursor: 'pointer'
+            // transform: 'scale(1.2 ,1.2)'
           };
           if (props.anySizePicked) {
             if (!props.pizzaSize[size]) {
               classes = size + ' NotClicked';
-              style['cursor'] = 'not-allowed';
+              style = {
+                cursor: 'not-allowed',
+                disabled: 'true',
+                filter: 'hue-rotate(180deg)',
+                borderRadius: '50%'
+              };
+            }
+          }
+          if (props.anySizePicked) {
+            if (props.pizzaSize[size]) {
+              classes = size + ' Clicked';
             }
           }
           return (
-            <div
+            // <div
+            //   className={classes}
+            //   onClick={() => props.pizzaSizeClicked(size)}
+            //   style={style}
+            //   key={size + i}
+            // >
+            //   <img src={PizzaLogo} alt={altMsg} />
+            // </div>
+            <input
+              type='image'
               className={classes}
+              src={PizzaLogo}
               onClick={() => props.pizzaSizeClicked(size)}
+              alt={altMsg}
               style={style}
-              key={size + i}
-            >
-              <img src={PizzaLogo} alt={altMsg} />
-            </div>
+            />
           );
         })}
       </div>
